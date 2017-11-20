73015fec547ab4525f6d7fde87052a85fe4785a6
Alexander Mextner <a-x-@users.noreply.github.com>
2017-11-20 11:10:12 +0300

    fix comment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var mapValues = require('lodash.mapvalues');
 /**
  * curry wrapper.
  * @example
- * const infl = Infl({ one: '{} apple' }, zero: 'no apples', some: '{} apples', many: '{} apples' })
+ * const infl = Infl({ one: '{} apple', zero: 'no apples', some: '{} apples', many: '{} apples' })
  * infl(100500) // --> '100500 apples'
  */
 module.exports = function(count, patterns)  {
