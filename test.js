1ddc311cd1b01d4608feb5bc717b33df8f21ce60
Alexander Mextner <a-x-@users.noreply.github.com>
2019-07-10 13:28:49 +0300

    Update test.js

--- test.js
+++ test.js
@@ -63,7 +63,7 @@ test('negative num', t => {
 })
 
 test('pattern-functions', t => {
-  t.is(inflect(11, { many: (value) => 'has b' + value.toString(2) + ' nerds in the team' }), 'nas b1011 nerds in the team');
+  t.is(inflect(11, { many: (value) => 'has b' + value.toString(2) + ' nerds in the team' }), 'has b1011 nerds in the team');
 });
 
 const infl = inflect({
